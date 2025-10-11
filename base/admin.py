f51070fdc9d6fb5ba2ba6bc3ff6269bdb2dced7a
tarvel <taiyebabatundejames@gmail.com>
2025-10-11 14:38:32 +0100

    fixed errors with admin not rendering

--- base/admin.py
+++ base/admin.py
@@ -1,23 +1,39 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
-
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = [
-        "email",
-        "is_staff",
-        "is_active",
-    ]
+    list_display = ['email', 'first_name', 'last_name', 'is_staff']
+    ordering = ('email',)
 
-    fieldsets = UserAdmin.fieldsets
-    fieldsets[1][1]["fields"] = ("first_name", "last_name", "email")
-    add_fieldsets = UserAdmin.add_fieldsets
-    add_fieldsets[0][1]["fields"] = ("email", "password", "password2")
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
 
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password", "password2"),
+        }),
+    )
 
 admin.site.register(CustomUser, CustomUserAdmin)
