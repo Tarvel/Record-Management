28873305b8ef2a595cc38628adf4447e65db9604
tarvel <taiyebabatundejames@gmail.com>
2025-10-11 14:29:23 +0100

    added new custom manager and custom user model to enable login with emailnot username

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = [
+        "email",
+        "is_staff",
+        "is_active",
+    ]
+
+    fieldsets = UserAdmin.fieldsets
+    fieldsets[1][1]["fields"] = ("first_name", "last_name", "email")
+    add_fieldsets = UserAdmin.add_fieldsets
+    add_fieldsets[0][1]["fields"] = ("email", "password", "password2")
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
