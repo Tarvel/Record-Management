0a9ea8acd391a4e06ba9acb77920c4a67fd46b9d
tarvel <taiyebabatundejames@gmail.com>
2025-10-14 19:32:38 +0100

    Updated build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
+
 set -e
 
-echo "Installing pip..."
+echo "installing pip"
 python3 -m pip install --upgrade pip
 
-echo "Installing requirements..."
-python3 -m pip install -r requirements.txt
+echo "installing requirements "
+pip install -r requirements.txt
 
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput
 
-# echo "Loading fixture data..."
-# python3 manage.py loaddata events.json
+echo "loading fixture data"
+python3 manage.py loaddata events.json
